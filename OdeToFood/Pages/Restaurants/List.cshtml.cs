4839c11ea0db3fa52e7bdb17e5032785822a60f6
Edin Jašarević <edinjasar14@gmail.com>
2020-11-04 14:40:29 +0100

    injectiong and using configuration

--- OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+        public string Message { get; set; }
+
+        public ListModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
+            Message = _configuration["Message"];
         }
     }
 }
