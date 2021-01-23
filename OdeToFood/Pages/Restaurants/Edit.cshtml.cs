2dbcabbf5f781742840a62d7d1d4af4f5eb37979
Edin Jašarević <edinjasar14@gmail.com>
2021-01-24 00:57:12 +0100

    building an edit form with tag halpers

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OdeToFood.Core;
 using OdeToFood.Data;
 
@@ -12,16 +13,20 @@ namespace OdeToFood.Pages.Restaurants
     public class EditModel : PageModel
     {
         private readonly IRestaurantData _restaurantData;
+        private readonly IHtmlHelper _htmlHelper;
 
         public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
 
-        public EditModel(IRestaurantData restaurantData)
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             _restaurantData = restaurantData;
+            _htmlHelper = htmlHelper;
         }
 
         public IActionResult OnGet(int restaurantId)
         {
+            Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
             Restaurant = _restaurantData.GetById(restaurantId);
             if (Restaurant == null)
             {
