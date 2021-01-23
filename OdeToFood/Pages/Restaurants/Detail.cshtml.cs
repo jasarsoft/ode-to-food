e4c4afb7a94dc8740a7d1449c6c3ddcf4b969d7f
Edin Jašarević <edinjasar14@gmail.com>
2021-01-24 00:01:55 +0100

    fetching restaurants id

--- OdeToFood/Pages/Restaurants/Detail.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -1,16 +1,22 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class DetailModel : PageModel
     {
+        private readonly IRestaurantData _restaurantData;
         public Restaurant Restaurant { get; set; }
+
+        public DetailModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
 
         public void OnGet(int restaurantId)
         {
-            Restaurant = new Restaurant();
-            Restaurant.Id = restaurantId;
+            Restaurant = _restaurantData.GetById(restaurantId);
         }
     }
 }
