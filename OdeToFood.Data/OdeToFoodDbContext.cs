25315228e73dbeef3950f9007689fa4eb511710e
Edin Jašarević <edinjasar14@gmail.com>
2021-01-28 20:27:45 +0100

    add configuration string and registering service

--- OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -5,6 +5,11 @@ namespace OdeToFood.Data
 {
     public class OdeToFoodDbContext: DbContext
     {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options) : base(options)
+        {
+            
+        }
+
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
