1ad5b3be940cf56c5e09e0de2807c45e9f63ad5f
Edin Jašarević <edinjasar14@gmail.com>
2021-01-28 19:36:09 +0100

    implementing an entity framework dbcontext

---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext: DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
