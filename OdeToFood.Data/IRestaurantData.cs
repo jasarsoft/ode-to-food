376509f0016b019a2734de2e3d4d6c58705cbb19
Edin Jašarević <edinjasar14@gmail.com>
2021-01-30 08:02:25 +0100

    implementing a data access service

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using OdeToFood.Core;
 
@@ -16,59 +15,8 @@ namespace OdeToFood.Data
 
         Restaurant Add(Restaurant newRestaurant);
 
+        Restaurant Delete(int id);
+
         int Commit();
-    }
-
-    public class InMemoryRestaurantData: IRestaurantData
-    {
-        readonly List<Restaurant> restaurants;
-
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>()
-            {
-                new Restaurant {Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
-                new Restaurant {Id = 2, Name = "Cinnamon Club", Location = "London", Cuisine = CuisineType.Indian},
-                new Restaurant {Id = 3, Name = "La Costa", Location = "California", Cuisine = CuisineType.Mexican},
-            };
-        }
-
-        public Restaurant GetById(int id)
-        {
-            return restaurants.SingleOrDefault(r => r.Id == id);
-        }
-
-        public Restaurant Update(Restaurant updateRestaurant)
-        {
-            var restaurant = restaurants.SingleOrDefault(p => p.Id == updateRestaurant.Id);
-            if (restaurant != null)
-            {
-                restaurant.Name = updateRestaurant.Name;
-                restaurant.Location = updateRestaurant.Location;
-                restaurant.Cuisine = updateRestaurant.Cuisine;
-            }
-
-            return restaurant;
-        }
-
-        public Restaurant Add(Restaurant newRestaurant)
-        {
-           restaurants.Add(newRestaurant);
-           newRestaurant.Id = restaurants.Max(p => p.Id) + 1;
-           return newRestaurant;
-        }
-
-        public int Commit()
-        {
-            return 0;
-        }
-
-        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
-        {
-            return from r in restaurants
-                where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                orderby r.Name
-                select r;
-        }
     }
 }
